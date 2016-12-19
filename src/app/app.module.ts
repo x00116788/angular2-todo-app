87e34bb2ffa4fe18c88f484c20bb94efd6db977b
suliat <x00116788@gmail.com>
2016-12-19 23:01:24 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { MyNewDirectiveDirective } from './my-new-directive.directive';
+import { MyNewPipePipe } from './my-new-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent,
+    MyNewDirectiveDirective,
+    MyNewPipePipe
   ],
   imports: [
     BrowserModule,
